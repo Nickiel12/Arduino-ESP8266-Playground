bd98366aea3f6a6ffc51f741bc3654247870289a
Nickiel <35903114+Nickiel12@users.noreply.github.com>
2019-08-01 11:50:40 -0700

    made it an open wifi

--- ESP 8266/wifi test/ESP Access Point/espAP.ino
+++ ESP 8266/wifi test/ESP Access Point/espAP.ino
@@ -1,14 +1,15 @@
 #include <ESP8266WiFi.h>        // Include the Wi-Fi library
 
 const char *ssid = "ESP8266 Access Point"; // The name of the Wi-Fi network that will be created
-const char *password = "thereisnospoon";   // The password required to connect to it, leave blank for an open network
+//const char *password = "password";   // The password required to connect to it, leave blank for an open network
 
 void setup() {
   Serial.begin(115200);
   delay(10);
   Serial.println('\n');
 
-  WiFi.softAP(ssid, password);             // Start the access point
+  //WiFi.softAP(ssid, password);             // Start the access point
+  WiFi.softAP(ssid);
   Serial.print("Access Point \"");
   Serial.print(ssid);
   Serial.println("\" started");
